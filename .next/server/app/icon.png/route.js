30e87368df4171111503e462e5a06a5c3016f991
Nathan Clevenger <nateclev@gmail.com>
2024-12-13 04:10:42 -0600

    Delete .next directory

--- .next/server/app/icon.png/route.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/icon.png/route.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/d44ad_next_f7d1c0._.js");
-runtime.loadChunk("server/chunks/[root of the server]__9dbf81._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.0_react-dom@18.3.1_react@18.3.1/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/icon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_document.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules__pnpm_f1b68e._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__e237d7._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.0_react-dom@18.3.1_react@18.3.1/node_modules/next/document.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
